a1ec4514af64fffa8a5da12585403d44b1667dd7
eric94nelson <eric94nelson@gmail.com>
2019-11-08 16:48:45 -0600

    Start server-based calculator app

--- CalculatorServer/calculator.js
+++ CalculatorServer/calculator.js
@@ -1,9 +1,11 @@
 //jshint esversion:6
 const express = require("express");
+
 const app = express();
-app.get("/", function(request, response){
-  console.log(request);
-  response.send("hello");
+
+app.get("/", function (req, res)
+{
+  response.send("Hello World");
 });
 
 app.listen(3000);
