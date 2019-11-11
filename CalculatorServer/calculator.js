0871da3df6a96ee638d83d3ce779c32c50a3e15d
eric94nelson <eric94nelson@gmail.com>
2019-11-11 13:27:28 -0600

    Complete basic server-side calculator operation website using nodemon, express, node, body-parser, bootstrap

--- CalculatorServer/calculator.js
+++ CalculatorServer/calculator.js
@@ -1,11 +1,22 @@
 //jshint esversion:6
 const express = require("express");
+const bp = require ("body-parser");
 
 const app = express();
+app.use(express.static("webpage"));
+app.use(bp.urlencoded({extended: true}));
 
 app.get("/", function (req, res)
 {
-  response.send("Hello World");
+  //C:/Users/eric9/OneDrive/Documents/webdev/CalculatorServer/webpage/
+  res.sendFile(__dirname+"/webpage/index.html");
+//  res.sendFile(__dirname+"/webpage/styles.css");
 });
 
-app.listen(3000);
+app.post("/", function(req, res){
+  res.send("The result is "+ (Number(req.body.num1) + Number(req.body.num2)));
+});
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000");
+});
